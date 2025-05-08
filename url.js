2ede337e3e5f8e3e34d89c6c8c64656b3f419a12
achun1721 <eva6932458@gmail.com>
2025-05-09 05:43:02 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeT1lVDCT6jNYno6frxw6Leve0oZVuHMwh8cOted6k4YVGR7g/viewform?usp=dialog',
+	'https://docs.google.com/forms/d/e/1FAIpQLScakPfM6ZlCt7S3HWjhVD2tUT9HuIm5o3d1Q_G2nSS9tIt-iQ/viewform?usp=dialog',
+	'https://docs.google.com/forms/d/e/1FAIpQLSeyegEr7p1RoNmqXh-tyssKYvNTrOQYTeqhWnJp-dBxtm83Cw/viewform?usp=dialog',
 ];
