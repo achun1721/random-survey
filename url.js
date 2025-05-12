cb25abb11ee6b3e5a0345fce1920618e7124496c
achun1721 <eva6932458@gmail.com>
2025-05-12 23:39:24 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSeT1lVDCT6jNYno6frxw6Leve0oZVuHMwh8cOted6k4YVGR7g/viewform?usp=dialog',
-	'https://docs.google.com/forms/d/e/1FAIpQLScakPfM6ZlCt7S3HWjhVD2tUT9HuIm5o3d1Q_G2nSS9tIt-iQ/viewform?usp=dialog',
-	'https://docs.google.com/forms/d/e/1FAIpQLSeyegEr7p1RoNmqXh-tyssKYvNTrOQYTeqhWnJp-dBxtm83Cw/viewform?usp=dialog',
+	'https://docs.google.com/forms/d/e/1FAIpQLSc8Lu-qPm_KoF5QFUY43njFPp7rJKDhizeuStVckDHOOGX0Qg/viewform?usp=dialog',
+  	'https://docs.google.com/forms/d/e/1FAIpQLSdEcsy4D6BaQdkvORpOcJYQSlwmo63qoRbiAJYr0pKqgaYADg/viewform?usp=dialog',
+  	'https://docs.google.com/forms/d/e/1FAIpQLSckuXDrLSBXf89b0WyIG14uJzdZ_KXmHqyKDJhHN8ySLB2prQ/viewform?usp=dialog',
 ];
